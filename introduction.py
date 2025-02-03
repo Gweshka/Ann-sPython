376fee82bf6668c20300b45b026cd9ef37a193da
Gweshka <kagwemaann@gmail.com>
2025-02-03 16:18:38 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,7 @@
+#print is a method used to display a value
+
+print("Ann")
+print("eMobilis")
+print(100)
+print(10 + 20)
+
---
+++ variable.py
@@ -0,0 +1,19 @@
+#variables is a container used to store values
+location = "Westlands"
+firstname = "Ann"
+lastname = "Kagwesh"
+school = "eMobilis"
+x = 39
+y = 2
+
+
+
+print(firstname)
+print(lastname)
+print(location)
+print(school)
+print(firstname,lastname)
+print("My names are",firstname,lastname)
+print(x,y)
+print("The Product of ",x,"and",y, "is 71")
+
