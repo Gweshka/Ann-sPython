7575e48d0900ce61790d2a316a564574f5654548
Gweshka <kagwemaann@gmail.com>
2025-02-04 15:53:24 +0300

    initial commit

---
+++ datatype.py
@@ -0,0 +1,31 @@
+number = 50 #integer
+num = 56.98 #float
+greeting = "Hello there" #string
+hasfeathers = True #boolean
+
+#data structures
+cars =["Mercedes","Toyota","Ferrari","Jeep"] #list
+fruits =("Apple","Banana","Orange",) #tuple
+Countries = {"England","France","germany"} #tung
+
+student = {
+  "firstname" : "Ann",
+  "Course": "MIT",
+  "Age": 19,
+  "nationality":"Kenyan"}
+
+
+
+print(number)
+print(num)
+print(greeting)
+print(cars)
+print(fruits)
+print(Countries)
+print(student)
+print(student["nationality"])
+
+
+#typecasting -is converting one data type to another
+print(float(number))
+print(int(num))
---
+++ string.py
@@ -0,0 +1,20 @@
+#is collection of characters
+text = "Hello there"
+course ="MIT"
+
+
+print(text)
+print(course)
+
+#accessing an element
+print(text[0])
+
+#size of a string
+print(len(text))
+
+#modifying a string
+print(text.upper())
+print(course.lower())
+
+#string concatenation
+print(text + " " + course)
